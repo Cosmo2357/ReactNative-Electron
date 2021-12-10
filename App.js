a0e91d02d68ccf45635ec219833ec298cd823f7f
yuhki666 <yuhki666@gmail.com>
2021-12-11 02:04:20 +0900

    nav

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-export default function App() {
+import Home from "./screen/Home";
+import Profile from "./screen/Profile";
+import Login from "./screen/Login";
+import Camera from "./screen/Camera";
+import AllPosts from "./screen/AllPosts";
+import KeyboardScreen from "./screen/Keyboard";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const TopTab = createMaterialTopTabNavigator();
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TopTab.Navigator>
+      <TopTab.Screen
+        name="Main"
+        component={Home}
+        options={{ tabBarLabel: "Home" }}
+      />
+      <TopTab.Screen
+        name="AllPosts"
+        component={AllPosts}
+        options={{ tabBarLabel: "AllPosts" }}
+      />
+      <TopTab.Screen
+        name="Keyboard"
+        component={KeyboardScreen}
+        options={{ tabBarLabel: "Keyboard" }}
+      />
+    </TopTab.Navigator>
+  );
+}
+function App() {
+  const [homeBadge, setHomeBadge] = useState(99);
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "person" : "person-outline";
+            } else if (route.name === "Login") {
+              iconName = focused ? "camera" : "camera-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={"black"} />;
+          },
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ tabBarBadge: homeBadge }}
+        />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Login" component={Login} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screen/Keyboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Keyboard() {
+  return (
+    <View>
+      <Text>Keyboard</Text>
+    </View>
+  );
+}
